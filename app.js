5e56759b301bf7789d44f9aaa653d3b417461ab2
Radhika <rkhe.37@gmail.com>
2020-11-03 10:43:24 +0530

    Final Update

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.controller('NotesController', function ($scope, $http) {
 	$scope.newNote = false;
 	$scope.addToDo = false;
 	$scope.addRow=1;
+    $scope.fullNote = false;
+    $scope.editingNote = false;
+    $scope.noteItemNumber = 0;
 	
 	$scope.noteList=[];
 	
@@ -28,14 +31,48 @@ app.controller('NotesController', function ($scope, $http) {
 		$scope.addRow = $scope.addRow - 1;
 		
 	};
+    
+    $scope.removenoteitem = function(noteItemNumber){
+        var index = $scope.noteList.indexOf(_.find($scope.noteList,function(o){return o.noteSerial == noteItemNumber;}));
+        $scope.noteList.splice(index,1);
+        $scope.numberOfNotes = $scope.noteList.length;
+    };
 	
 	$scope.saveNote = function(noteTitleValue,noteDescrValue){
-		$scope.note={};
-		$scope.note.noteTitle = noteTitleValue;
-		$scope.note.noteDescription = noteDescrValue;
-		$scope.noteList.push($scope.note);
+        if(!$scope.editingNote){
+            $scope.note={};
+            $scope.note.noteSerial=$scope.numberOfNotes;
+            $scope.note.noteTitle = noteTitleValue;
+            $scope.note.noteDescription = noteDescrValue;
+            $scope.noteList.push($scope.note);
+            $scope.numberOfNotes += 1;
+            $scope.note={};
+        }
+        else{
+           $scope.note = _.find($scope.noteList,function(o){return o.noteSerial == $scope.noteItemNumber;});
+           $scope.note.noteTitle = noteTitleValue;
+           $scope.noteDescription = noteDescrValue;
+           $scope.numberOfNotes = $scope.noteList.length;
+           $scope.note={};
+        }
 		$scope.menuOpened=true;
-		$scope.numberOfNotes += 1; 
-	}
+	};
+    
+    
+    $scope.viewFullNote = function(noteItemNumber){
+        if(!$scope.fullNote) $scope.fullNote = true;
+        else $scope.fullNote = false;
+        $scope.note = _.find($scope.noteList,function(o){return o.noteSerial == noteItemNumber;});
+        return $scope.note;
+    };
+    
+    $scope.editNote = function(noteItemNumber){
+        $scope.note = _.find($scope.noteList,function(o){return o.noteSerial == noteItemNumber;});
+        $scope.noteTitleValue = $scope.note.noteTitle;
+        $scope.noteDescrValue = $scope.note.noteDescription;
+        $scope.editingNote=true;
+        $scope.noteItemNumber = noteItemNumber;
+        $scope.addNote();
+    };
 	
 });
